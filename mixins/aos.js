01e4fb42725e9afbe3cab613ab588c9a3c9a905c
bartoginski <bartoginski@outlook.com>
2021-11-08 15:11:08 +0100

    Add aos to the project

---
+++ mixins/aos.js
@@ -0,0 +1,11 @@
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+// Fade animations
+// https://www.geekco.fr/posts/en/how-to-integrate-aos-in-your-nuxt-project
+
+export default {
+  mounted() {
+    AOS.init({ })
+  }
+}
