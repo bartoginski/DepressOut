2910631ee4ef214bece17919cb65ee663f252b78
bartoginski <bartoginski@outlook.com>
2021-12-03 00:44:10 +0100

    Create layout of index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <Psycha />
+  <main>
+    <h1 data-aos="fade-down" class="text-center text-5xl sm:text-8xl font-bold tracking-wide py-40">Psycha is sitting</h1>
+    <div>
+      <img src="~/assets/img/wave_top.svg" alt="">
+      <!-- temporary set height for div -->
+      <div class="h-screen bg-primary">
+        <!-- Add sections below -->
+        <!-- section 1 -->
+        <!-- section 2 -->
+      </div>
+      <img src="~/assets/img/wave_bottom.svg" alt="">
+    </div>
+    <!-- section 3 -->
+  </main>
+
 </template>
 
 <script>
-export default {}
+import aosMixin from '~/mixins/aos'
+export default {
+  mixins: [aosMixin]
+}
 </script>
