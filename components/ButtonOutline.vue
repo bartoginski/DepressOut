80c7b1e6b87dbaae6bec0014a5076ff3a0bea807
jakubstolarczyk14 <78145134+jakubstolarczyk14@users.noreply.github.com>
2021-12-03 19:43:05 +0100

    Update buttons

---
+++ components/ButtonOutline.vue
@@ -0,0 +1,5 @@
+<template>
+<button class="p-3 text-primary bg-white border-2 border-primary rounded-2xl">
+    Tworzymy
+</button>
+</template>
---
+++ components/ButtonPrimary.vue
@@ -0,0 +1,5 @@
+<template>
+<button class="p-3 text-white bg-primary rounded-2xl">
+      Tworzymy
+</button>
+</templete>
