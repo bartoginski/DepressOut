ccc7b987afe09de3e23ce2bbd29f504f6ccdad48
bartoginski <bartoginski@outlook.com>
2021-12-03 20:25:40 +0100

    Fix cookiesbar position

--- components/CookiesBar.vue
+++ components/CookiesBar.vue
@@ -1,3 +1,3 @@
 <template>
-    <div class="bg-light-gray p-5">Ta strona używa ciasteczek by zapewnić ci najlepsze mozliwe doświadczenia <ButtonPrimary/> <ButtonOutline/></div>
+    <div class="bg-light-gray p-5 fixed z-50 w-full bottom-0">Ta strona używa ciasteczek by zapewnić ci najlepsze mozliwe doświadczenia <ButtonPrimary/> <ButtonOutline/></div>
 </template>
