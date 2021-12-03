c67948a7070c366e86ebfad0938448a2783077ad
jakubstolarczyk14 <78145134+jakubstolarczyk14@users.noreply.github.com>
2021-12-03 20:31:44 +0100

    Update xd

--- components/ButtonOutline.vue
+++ components/ButtonOutline.vue
@@ -1,5 +1,5 @@
 <template>
-<button class="p-3 text-primary bg-white border-2 border-primary rounded-2xl">
+<button class="p-3 text-primary bg-transparent border-2 border-primary rounded-2xl">
     Tworzymy
 </button>
 </template>
--- components/ButtonPrimary.vue
+++ components/ButtonPrimary.vue
@@ -1,5 +1,5 @@
 <template>
-<button class="p-3 text-white bg-primary border-2 border-primary rounded-2xl">
+<button class="p-3 text-white bg-primary border-2 border-transparent rounded-2xl">
     Tworzymy
 </button>
 </template>
