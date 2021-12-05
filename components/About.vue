70a7447da4b09287163326cd9106e4eb6836668e
Oski-Boski <78144068+Oski-Boski@users.noreply.github.com>
2021-12-05 13:05:39 +0100

    update about

---
+++ components/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container flex">
+    <div class="text-white flex-1">
+      <h2 class="text-6xl">Trochę o nas</h2>
+      <p class="text-xl">
+        Tworzymy projekt mający na celu pomóc ludziom dotkniętym depresją.
+        Dlatego stworzyliśmy ankietę by lepiej zrozumieć problem. Dziękujemy za
+        poświęcony czas.
+      </p>
+    </div>
+    <div class="flex-1">
+      <img src="~/assets/img/log.svg" alt="" />
+    </div>
+  </div>
+</template>
