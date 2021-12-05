268ffb273e22887acae554fbd360f2d6afad302c
bartoginski <bartoginski@outlook.com>
2021-12-05 13:59:39 +0100

    Add blog posts

---
+++ content/articles/second-post.md
@@ -0,0 +1,13 @@
+---
+title: My second Blog Post
+description: Learning how to do anything
+img: first.svg
+alt: my first blog post
+tag: Deprejsa
+author: Mateusz Jaromin
+---
+
+
+# My first blog post
+
+Second post btw
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>Index blog page</h1>
+  <WaveSection>
+    <div class="container grid grid-cols-2 gap-x-24 gap-y-32">
+      <BlogCard
+        v-for="article in articles"
+        :key="article.slug"
+        :article="article"
+      />
+      <!-- <pre>{{ article }}</pre> -->
+    </div>
+  </WaveSection>
 </template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const articles = await $content('articles', params.slug)
+      .sortBy('createdAt', 'desc')
+      .fetch()
+    return { articles }
+  },
+}
+</script>
