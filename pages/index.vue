eb119429c70917ceffab2215553259469efdbe90
bartoginski <bartoginski@outlook.com>
2021-12-05 12:01:43 +0100

    Update animation of h1 entry

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,30 @@
 <template>
   <main>
-    <h1 data-aos="fade-down" class="text-center text-5xl sm:text-8xl font-bold tracking-wide py-40">Psycha is sitting</h1>
+    <h1
+      data-aos-delay="300"
+      data-aos="fade-down"
+      class="text-center text-5xl sm:text-8xl font-bold tracking-wide py-40"
+    >
+      Psycha is sitting
+    </h1>
     <div>
-      <img src="~/assets/img/wave_top.svg" alt="" class="w-screen">
+      <img src="~/assets/img/wave_top.svg" alt="" class="w-screen" />
       <!-- temporary set height for div -->
       <div class="h-screen bg-primary">
         <!-- Add sections below -->
         <!-- section 1 -->
         <!-- section 2 -->
       </div>
-      <img src="~/assets/img/wave_bottom.svg" alt="" class="w-screen">
+      <img src="~/assets/img/wave_bottom.svg" alt="" class="w-screen" />
     </div>
     <!-- section 3 -->
-    <!-- cookies bar ponieżej -->
-    <CookiesBar/>
+    <CookiesBar />
   </main>
-
 </template>
 
 <script>
 import aosMixin from '~/mixins/aos'
 export default {
-  mixins: [aosMixin]
+  mixins: [aosMixin],
 }
 </script>
