2babd6e50d35f0b3b0895bf2dcc39ef8b9b5c7d4
bartoginski <bartoginski@outlook.com>
2021-12-06 10:29:52 +0100

    Update components

--- components/WaveSection.vue
+++ components/WaveSection.vue
@@ -1,6 +1,6 @@
 <template>
  <div>
-      <img src="~/assets/img/wave_top.svg" alt="" class="w-screen" />
+      <img src="~/assets/img/wave_top.svg" alt="" class="w-screen -m-px" />
       <!-- temporary set height for div -->
       <div class="flex flex-col gap-20 md:gap-40 py-10 md:py-0 bg-primary">
           <slot></slot>
--- components/global/CookiesBar.vue
+++ components/global/CookiesBar.vue
@@ -1,15 +1,55 @@
 <template>
   <div
     v-if="isDisplayed"
-    class="bg-light-gray p-3 md:px-10 fixed z-50 w-full bottom-0 text-xl flex justify-between md:justify-evenly text-white items-center"
+    class="
+      bg-light-gray
+      p-3
+      md:px-10
+      fixed
+      z-50
+      w-full
+      bottom-0
+      text-xl
+      flex
+      justify-between
+      md:justify-evenly
+      text-white
+      items-center
+      gap-2
+      md:gap-0
+    "
   >
-    <span class="py-0 px-5 md:px-10 md:py-4 text-center md:text-xl text-xl">
-    Ta strona używa ciasteczek by zapewnić ci najlepsze mozliwe doświadczenia
+    <span class="py-0 md:px-10 md:py-4 text-center md:text-xl text-xl">
+      Ta strona używa ciasteczek by zapewnić ci najlepsze mozliwe doświadczenia
     </span>
     <div class="flex justify-evenly gap-5 md:gap-14 flex-col md:flex-row">
-    <ButtonPrimary class="text-sm md:text-xl py-2 px-10 md:px-8 whitespace-nowrap md:whitespace-normal" :onclick="closeBar">Zgadzam się</ButtonPrimary>
-    <ButtonOutline class="text-sm md:text-xl py-2 px-10 md:px-8 whitespace-nowrap md:whitespace-normal" :onclick="() => {}">Więcej informacji</ButtonOutline>
-  </div>
+      <ButtonPrimary
+        class="
+          text-sm
+          md:text-xl
+          py-2
+          px-10
+          md:px-8
+          whitespace-nowrap
+          md:whitespace-normal
+        "
+        :onclick="closeBar"
+        >Zgadzam się</ButtonPrimary
+      >
+      <ButtonOutline
+        class="
+          text-sm
+          md:text-xl
+          py-2
+          px-10
+          md:px-8
+          whitespace-nowrap
+          md:whitespace-normal
+        "
+        :onclick="() => {}"
+        >Więcej informacji</ButtonOutline
+      >
+    </div>
   </div>
 </template>
 
