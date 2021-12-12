c2a23dca432f7d79a5a762a1473fbec9a71b9963
adamaxt <adamaxt1@gmail.com>
2021-12-12 16:09:12 +0100

    Update CTA.vue
    dodałem przycisk, ale nie potrafie tego ostylować

--- components/global/CTA.vue
+++ components/global/CTA.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="px-60">
-    <div class="px-20 text-white transition duration-500 ease-in-out bg-primary border-2 border-primary rounded-2xl">
-        <div>
-        <h1 class="text-xl">Jesteśmy tu, żeby ci pomóc</h1><br />
-        Potrzebujesz pomoy, a nie wiesz gdzie jej szukać?<br />
-        Sprawdź nasze telefony pomocy
+        <div class="px-20 py-2 text-white transition duration-500 ease-in-out bg-primary border-2 border-primary rounded-2xl">
+            <div>
+                <h1 class="text-xl">Jesteśmy tu, żeby ci pomóc</h1><br />
+                    
+                Potrzebujesz pomoy, a nie wiesz gdzie jej szukać?
+                    <div class="text center flex justify-end">
+                        <ButtonPrimary class="border-white bg-transparent ">Sprawdź</ButtonPrimary>
+                    </div>
+                    <br />
+                
+                Sprawdź nasze telefony pomocy
+            </div>
         </div>
-        <div>
-            <ButtonPrimary />
-        </div>
-    </div></div>
+    </div>
 </template>
