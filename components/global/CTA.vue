5e4ab1bd593e1a7d09440361fde1228afc2dec05
adamaxt <adamaxt1@gmail.com>
2021-12-13 18:42:49 +0100

    Update CTA.vue
    Nie rosponsywny, bez kółek w tle bo sie rozpierdalały te kółka jak je robiłem za każdym razem

--- components/global/CTA.vue
+++ components/global/CTA.vue
@@ -1,16 +1,15 @@
 <template>
     <div class="px-60">
-        <div class="px-20 py-2 text-white transition duration-500 ease-in-out bg-primary border-2 border-primary rounded-2xl">
+        <div class="container px-20 py-2 text-white transition duration-500 ease-in-out bg-primary border-2 border-primary rounded-2xl flex justify-between justify-items-center">
             <div>
                 <h1 class="text-xl">Jesteśmy tu, żeby ci pomóc</h1><br />
-                    
-                Potrzebujesz pomoy, a nie wiesz gdzie jej szukać?
-                    <div class="text center flex justify-end">
+                <p>
+                    Potrzebujesz pomoy, a nie wiesz gdzie jej szukać?<br />
+                    Sprawdź nasze telefony pomocy
+                </p>
+            </div>
+            <div class="py-10 px-20">
                         <ButtonPrimary class="border-white bg-transparent ">Sprawdź</ButtonPrimary>
-                    </div>
-                    <br />
-                
-                Sprawdź nasze telefony pomocy
             </div>
         </div>
     </div>
