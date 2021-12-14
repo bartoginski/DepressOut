07400fd7ab9b3941d6b4c83c96d6bb1d347f8be8
Oski-Boski <78144068+Oski-Boski@users.noreply.github.com>
2021-12-14 22:28:35 +0100

    Update CTA.vue

--- components/global/CTA.vue
+++ components/global/CTA.vue
@@ -33,8 +33,8 @@
       alt=""
       class="absolute z-0 -right-2 -bottom-10"
     />
-    <div class="flex flex-col gap-10 justify-center align-middle z-10">
-      <h2 class="text-4xl font-bold text-center">Jesteśmy tu, żeby pomóc</h2>
+    <div class="flex flex-col gap-10 z-10">
+      <h2 class="text-4xl font-bold">Jesteśmy tu, żeby pomóc</h2>
       <div class="text-xl justify-left">
         <p>Potrzebujesz pomocy, a nie wiesz gdzie jej szukać?</p>
         <p>Sprawdź nasze telefony zaufania</p>
