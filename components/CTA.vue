9c389711df5d09299638b2b02a6f4613bb3b39c4
bartoginski <bartoginski@outlook.com>
2021-12-14 23:38:56 +0100

    Fix cta, add new button variant

--- components/CTA.vue
+++ components/CTA.vue
@@ -1,49 +1,46 @@
 <template>
-  <div
-    class="
-      md:px-12 md:py-10 
-      py-4
-      px-6
-      my-20
-      mx-auto
-      text-white
-      bg-primary
-      rounded-2xl
-      md:max-w-max
-      w-3/4
-      flex
-      md:flex-row
-      flex-col
-      md:gap-24
-      gap-8
-      align-middle
-      justify-center
-      relative
-      overflow-hidden
-      before:absolute before:w-20 before:h-20 before:bg-white before:top-0
-    "
-  >
-    <img
-      src="~/assets/img/koloo.svg"
-      alt=""
-      class="absolute z-0 -left-1 -top-2"
-    />
-    <img
-      src="~/assets/img/kolo.svg"
-      alt=""
-      class="absolute z-0 -right-2 -bottom-10"
-    />
-    <div class="flex flex-col gap-10 z-10">
-      <h2 class="text-4xl font-bold">Jesteśmy tu, żeby pomóc</h2>
-      <div class="text-xl justify-left">
-        <p>Potrzebujesz pomocy, a nie wiesz gdzie jej szukać?</p>
-        <p>Sprawdź nasze telefony zaufania</p>
+  <div class="container mt-40">
+    <div
+      class="
+        text-white
+        bg-primary
+        px-14 
+        py-8
+        rounded-2xl
+        flex
+        md:flex-row
+        flex-col
+        md:gap-24
+        gap-8
+        justify-center
+        md:justify-between
+        relative
+        overflow-hidden
+        before:absolute before:w-20 before:h-20 before:bg-white before:top-0
+      "
+    >
+      <img
+        src="~/assets/img/koloo.svg"
+        alt=""
+        class="absolute z-0 -left-1 -top-2"
+      />
+      <img
+        src="~/assets/img/kolo.svg"
+        alt=""
+        class="absolute z-0 -right-2 -bottom-10"
+      />
+      <div class="flex flex-col gap-10 z-10 text-center md:text-left">
+        <h2 class="text-4xl font-bold">Jesteśmy tu, żeby pomóc</h2>
+        <div class="text-xl justify-left">
+          <p>Potrzebujesz pomocy, a nie wiesz gdzie jej szukać?</p>
+          <p>Sprawdź nasze telefony zaufania</p>
+        </div>
       </div>
-    </div>
-    <div class="py-10 px-20 text-xl z-0">
-      <ButtonPrimary class="border-white bg-transparent px-14"
-        >Sprawdź</ButtonPrimary
-      >
+      <div class="flex justify-center items-center text-xl z-0">
+        <ButtonOutlineWhite class="border-white bg-transparent px-14 lg:mr-14 lg:w-72"
+          >Sprawdź</ButtonOutlineWhite
+        >
+      </div>
     </div>
   </div>
 </template>
---
+++ components/buttons/ButtonOutlineWhite.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    class="p-3 text-white transition duration-500 ease-in-out hover:bg-gray-900 bg-transparent border-2 border-white rounded-2xl"
+    @click="onclick"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    onclick: {
+      type: Function,
+      required: false,
+      default: () => {}
+    },
+  },
+}
+</script>
