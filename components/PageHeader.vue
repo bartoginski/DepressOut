872962965e5019b9f4dd2148fed5926a77e3d3dd
bartoginski <bartoginski@outlook.com>
2021-12-15 10:52:10 +0100

    Add darkmode btn

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -2,7 +2,7 @@
   <h1
     data-aos-delay="300"
     data-aos="fade-down"
-    class="text-center text-5xl sm:text-8xl font-bold tracking-wide py-40"
+    class="text-center text-5xl sm:text-8xl font-bold tracking-wide py-40 dark:text-white"
   >
     <slot></slot>
   </h1>
