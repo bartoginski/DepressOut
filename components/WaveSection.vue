1d62db1bb63cf5f05eb66303f033ec5ec5d92b0a
bartoginski <bartoginski@outlook.com>
2021-12-15 10:15:17 +0100

    Fix wave

--- components/WaveSection.vue
+++ components/WaveSection.vue
@@ -1,11 +1,11 @@
 <template>
  <div>
-      <img src="~/assets/img/wave_top.svg" alt="" class="w-screen -m-px" />
+      <img src="~/assets/img/wave_top.svg" alt="" class="w-screen wave__top" />
       <!-- temporary set height for div -->
       <div class="flex flex-col gap-20 md:gap-40 py-10 md:py-0 bg-primary">
           <slot></slot>
       </div>
-      <img src="~/assets/img/wave_bottom.svg" alt="" class="w-screen -m-px" />
+      <img src="~/assets/img/wave_bottom.svg" alt="" class="w-screen wave__bottom" />
     </div>
 </template>
 
@@ -16,5 +16,13 @@
 </script>
 
 <style lang="scss" scoped>
+    .wave {
+        &__top {
+            margin-bottom: -1px;
+        }
+        &__bottom {
+            margin-top: -1px;
+        }
+    }
 
 </style>
