f2b13198d6a6d804a554f67ca780a88e835f0d6e
Gopnik88 <filipmarczyk@gmail.com>
2021-12-17 00:08:00 +0100

    Update phones.vue
    phones

--- pages/phones.vue
+++ pages/phones.vue
@@ -9,16 +9,16 @@
           md:items-center
           gap-0
           md:gap-20
-          mt-20
+          mt-10 
           mb-20
         "
       >
         <div
           data-aos-delay="300"
           data-aos="fade-down"
-          class="text-black text-center md:text-left container"
+          class="text-black text-center md:text-left container mt-0 mb-20"
         >
-          <h2 class="text-4xl md:text-6xl text-center">
+          <h2 class="text-4xl md:text-6xl text-center mt-0 mb-20">
             Skorzystaj z bazy numerów telefonów.
           </h2>
         </div>
@@ -50,30 +50,30 @@
               md:gap-0
             "
           >
-            <div class="text-xl flex flex-col gap-10">
-              <p>
-                Antydepresyjny Telefon Forum Przeciw Depresji<br />
-                tel. 22 594 91 00<br />
-                (czynny w każdą środę – czwartek od 17.00 do 19.00) koszt
-                połączenia jak na numer stacjonarny wg taryfy Twojego operatora
-                telefonicznego
+            <div class="text-3xl flex flex-col gap-10">
+              <p class='font-bold'>
+                <span class='font-black'>Antydepresyjny Telefon Forum Przeciw Depresji</span><br />
+                <span class='text-gray-500'><a class='hover:text-primary' target='_blank' href="https://forumprzeciwdepresji.pl/antydepresyjny-telefon">tel. 22 594 91 00</a><br />
+                (czynny w każdą środę – czwartek od 17.00 do 19.00)<br/>
+                koszt połączenia jak na numer stacjonarny wg taryfy Twojego operatora telefonicznego</span>
               </p>
 
-              <p>
-                Telefon zaufania dla osób dorosłych w kryzysie emocjonalnym<br />
-                tel. 116 123<br />
-                (poniedziałek-piątek od 14:00-22:00, połączenie bezpłatne)
+              <p class='font-bold'>
+                <span class='font-black'>Telefon zaufania dla osób dorosłych w kryzysie emocjonalnym</span><br />
+                <span class='text-gray-500'><a class='hover:text-primary' target='_blank' href="https://psychologia.edu.pl/116123.html">tel. 116 123</a><br />
+                (poniedziałek-piątek od 14:00-22:00, połączenie bezpłatne)</span>
               </p>
 
-              <p>
-                Telefon Zaufania dla Dzieci i Młodzieży<br />
-                tel. 116 111<br />
-                (czynny 7 dni w tygodniu, 24 h na dobę)
+              <p class='font-bold'>
+                <span class='font-black'>Telefon Zaufania dla Dzieci i Młodzieży</span><br />
+                <span class='text-gray-500'><a class='hover:text-primary' target='_blank' href="https://116111.pl/">tel. 116 111</a><br />
+                (czynny 7 dni w tygodniu, 24 h na dobę)</span>
               </p>
 
-              <p>
-                ITAKA – Antydepresyjny telefon zaufania tel. 22 484 88 01 Dyżury
-                psychologów:<br />
+              <p class='font-bold'>
+                <span class='font-black'>ITAKA – Antydepresyjny telefon zaufania</span><br />
+                <span class='text-gray-500'><a class='hover:text-primary' target='_blank' href="https://stopdepresji.pl/antydepresyjny-telefon-zaufania/">tel. 22 484 88 01</a><br />
+                Dyżury psychologów:<br />
                 poniedziałek 15.00 – 20.00<br />
                 wtorek 15:00 – 20:00<br />
                 środa 10.00 – 15.00<br />
@@ -81,14 +81,13 @@
                 niedziela 15:00 – 20:00<br />
                 Dyżury psychiatrów:<br />
                 czwartek 16:00 – 21:00<br />
-                piątek 15.00 – 20.00<br />
+                piątek 15.00 – 20.00<br /></span>
               </p>
 
-              <p>
-                Telefoniczna Pierwsza Pomoc Psychologiczna<br />
-                tel. 22 425 98 48<br />
-                (poniedziałek – piątek od 17:00 do 20:00 / sobota od 15:00 do
-                17:00)
+              <p class='font-bold'>
+                <span class='font-black'>Telefoniczna Pierwsza Pomoc Psychologiczna</span><br />
+                <span class='text-gray-500'><a class='hover:text-primary' target='_blank' href="https://www.qzmianom.org/index.php?page=Aktualnosci&id=3&SSID=2bf2cb03347e31cfe731a8eb8104c3a7&SSID=2bf2cb03347e31cfe731a8eb8104c3a7">tel. 22 425 98 48</a><br />
+                (poniedziałek – piątek od 17:00 do 20:00 / sobota od 15:00 do 17:00)</span>
               </p>
             </div>
           </div>
