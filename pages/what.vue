7e539ab9f61dd5add043d15e60b9a59146136d4d
xDeJwiD <dejwid.sz.yt@gmail.com>
2021-12-19 14:12:43 +0100

    What.vue

---
+++ pages/what.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <PageHeader> Co i jak? </PageHeader>
+    <WaveSection>
+      <Why />
+      <WhatOffers />
+    </WaveSection>
+      <GetHelp />
+  </main>
+</template>
