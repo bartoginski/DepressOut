330a227953a55b97c2f7a0f9994893383a42f5a5
jakubstolarczyk14 <78145134+jakubstolarczyk14@users.noreply.github.com>
2021-12-22 14:19:36 +0100

    Rectangle

---
+++ components/sections/Depressionv4.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+      <div
+        class="container flex-col flex md:items-center gap-0 md:gap-20 mt-0 mb-0 "
+      >
+            <div class="text-3xl flex flex-col gap-10">
+              <p class="font-bold">
+                <span class="text-white"
+                  >Według Światowej Organizacji Zdrowia (WHO) depresja jest wiodącą przyczyną niesprawności i niezdolności do pracy na świecie oraz najczęściej spotykanym zaburzeniem psychicznym. Choruje na nią w ciągu całego życia kilkanaście procent populacji osób dorosłych. Choroba ta dotyka dwa razy częściej kobiet. Co dziesiąty pacjent zgłaszający się do lekarza pierwszego kontaktu z powodu innych dolegliwości ma pełnoobjawową depresję, a drugie tyle osób cierpi z powodu pojedynczych objawów depresyjnych. Niestety ponad połowa tych przypadków pozostaje nierozpoznana, a spośród osób z rozpoznaną depresją jedynie połowa otrzymuje adekwatne leczenie. Z tego względu niezwykle ważną dla każdego (nie tylko dla lekarza) kwestią jest znajomość natury i objawów depresji oraz podstaw jej diagnostyki i leczenia.
+
+                </span
+                ><br />
+              </p>
+              <p class="font-bold">
+                <span class="text-white"
+                  >1,5 mln osób w Polsce cierpi na depresję, jest to aż 4% populacji Polski
+                </span
+                ><br />
+              </p>
+            </div>
+          </div>
+    </section>
+</template>
