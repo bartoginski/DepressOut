ccdff22250dec96ce3b77212a5483edc4d473f44
bartoginski <bartoginski@outlook.com>
2021-12-23 14:32:14 +0100

    Update phones page

--- pages/phones.vue
+++ pages/phones.vue
@@ -1,136 +1,106 @@
 <template>
   <main>
     <PageHeader> Telefon Pomocy </PageHeader>
-    <section>
-      <div
-        class="container flex-col flex md:items-center gap-0 md:gap-20 mt-0 mb-0 "
-      >
-        <div
-          data-aos-delay="300"
-          data-aos="fade-down"
-          class="text-black text-center md:text-left container mt-0 mb-0"
-        >
-          <h2 class="text-4xl md:text-6xl text-center mt-0 mb-0">
-            Skorzystaj z bazy numerów telefonów.
-          </h2>
-        </div>
-        <div
-          class="gap-0 md:gap-40 text-center md:text-left flex-1 justify-center md:justify-end flex-row flex md:items-left mb-0"
-        >
-          <div
-            data-aos-delay="300"
-            data-aos="fade-right"
-            class="text-slate-300 text-center md:text-left flex-1 justify-center md:justify-end flex-row flex md:items-left gap-4 md:gap-0"
+    <SectionWrapper>
+      <template #content>
+        <p class="font-bold">
+          <span class="font-black"
+            >Antydepresyjny Telefon Forum Przeciw Depresji</span
+          ><br />
+          <span class="text-gray-500"
+            ><a
+              class="text-primary hover:opacity-70 transition-opacity"
+              target="_blank"
+              href="https://forumprzeciwdepresji.pl/antydepresyjny-telefon"
+              >tel. 22 594 91 00</a
+            ><br />
+            Czynny w każdą środę oraz czwartek od 17.00 do 19.00<br />
+            <span class="text-gray-400"
+              >(koszt połączenia jak na numer stacjonarny wg taryfy operatora
+              telefonicznego)</span
+            ></span
           >
-            <div class="text-3xl flex flex-col gap-10">
-              <p class="font-bold">
-                <span class="font-black"
-                  >Antydepresyjny Telefon Forum Przeciw Depresji</span
-                ><br />
-                <span class="text-gray-500"
-                  ><a
-                    class="text-primary hover:opacity-70 transition-opacity"
-                    target="_blank"
-                    href="https://forumprzeciwdepresji.pl/antydepresyjny-telefon"
-                    >tel. 22 594 91 00</a
-                  ><br />
-                  Czynny w każdą środę oraz czwartek od 17.00 do 19.00<br />
-                  <span class="text-gray-400"
-                    >(koszt połączenia jak na numer stacjonarny wg taryfy
-                    operatora telefonicznego)</span
-                  ></span
-                >
-              </p>
+        </p>
 
-              <p class="font-bold">
-                <span class="font-black"
-                  >Telefon zaufania dla osób dorosłych w kryzysie
-                  emocjonalnym</span
-                ><br />
-                <span class="text-gray-500"
-                  ><a
-                    class="text-primary hover:opacity-70 transition-opacity"
-                    target="_blank"
-                    href="https://psychologia.edu.pl/116123.html"
-                    >tel. 116 123</a
-                  ><br />
-                  Poniedziałek-piątek od 14:00 do 22:00<br />
-                  <span class="text-gray-400"
-                    >(połączenie bezpłatne)</span
-                  ></span
-                >
-              </p>
+        <p class="font-bold">
+          <span class="font-black"
+            >Telefon zaufania dla osób dorosłych w kryzysie emocjonalnym</span
+          ><br />
+          <span class="text-gray-500"
+            ><a
+              class="text-primary hover:opacity-70 transition-opacity"
+              target="_blank"
+              href="https://psychologia.edu.pl/116123.html"
+              >tel. 116 123</a
+            ><br />
+            Poniedziałek-piątek od 14:00 do 22:00<br />
+            <span class="text-gray-400">(połączenie bezpłatne)</span></span
+          >
+        </p>
 
-              <p class="font-bold">
-                <span class="font-black"
-                  >Telefon Zaufania dla Dzieci i Młodzieży</span
-                ><br />
-                <span class="text-gray-500"
-                  ><a
-                    class="text-primary hover:opacity-70 transition-opacity"
-                    target="_blank"
-                    href="https://116111.pl/"
-                    >tel. 116 111</a
-                  ><br />
-                  Czynny 7 dni w tygodniu, 24 h na dobę<br />
-                  <span class="text-gray-400"
-                    >(połączenie bezpłatne)</span
-                  ></span
-                >
-              </p>
+        <p class="font-bold">
+          <span class="font-black">Telefon Zaufania dla Dzieci i Młodzieży</span
+          ><br />
+          <span class="text-gray-500"
+            ><a
+              class="text-primary hover:opacity-70 transition-opacity"
+              target="_blank"
+              href="https://116111.pl/"
+              >tel. 116 111</a
+            ><br />
+            Czynny 7 dni w tygodniu, 24 h na dobę<br />
+            <span class="text-gray-400">(połączenie bezpłatne)</span></span
+          >
+        </p>
 
-              <p class="font-bold">
-                <span class="font-black"
-                  >ITAKA – Antydepresyjny telefon zaufania</span
-                ><br />
-                <span class="text-gray-500"
-                  ><a
-                    class="text-primary hover:opacity-70 transition-opacity"
-                    target="_blank"
-                    href="https://stopdepresji.pl/antydepresyjny-telefon-zaufania/"
-                    >tel. 22 484 88 01</a
-                  ><br />
-                  Dyżury psychologów:<br />
-                  Poniedziałek 15:00 – 20:00<br />
-                  Wtorek 15:00 – 20:00<br />
-                  Piątek 15:00 – 29:00<br />
-                  Dyżury seksuologów:<br />
-                  Środa 15:00-20:00<br />
-                  Dyżury psychiatrów:<br />
-                  czwartek 15:00 – 20:00<br />
-                </span>
-              </p>
+        <p class="font-bold">
+          <span class="font-black">ITAKA – Antydepresyjny telefon zaufania</span
+          ><br />
+          <span class="text-gray-500"
+            ><a
+              class="text-primary hover:opacity-70 transition-opacity"
+              target="_blank"
+              href="https://stopdepresji.pl/antydepresyjny-telefon-zaufania/"
+              >tel. 22 484 88 01</a
+            ><br />
+            Dyżury psychologów:<br />
+            Poniedziałek 15:00 – 20:00<br />
+            Wtorek 15:00 – 20:00<br />
+            Piątek 15:00 – 29:00<br />
+            Dyżury seksuologów:<br />
+            Środa 15:00-20:00<br />
+            Dyżury psychiatrów:<br />
+            czwartek 15:00 – 20:00<br />
+          </span>
+        </p>
 
-              <p class="font-bold">
-                <span class="font-black"
-                  >Telefoniczna Pierwsza Pomoc Psychologiczna</span
-                ><br />
-                <span class="text-gray-500"
-                  ><a
-                    class="text-primary hover:opacity-70 transition-opacity"
-                    target="_blank"
-                    href="https://www.qzmianom.org/index.php?page=Aktualnosci&id=3&SSID=2bf2cb03347e31cfe731a8eb8104c3a7&SSID=2bf2cb03347e31cfe731a8eb8104c3a7"
-                    >tel. 22 425 98 48</a
-                  ><br />
-                  Poniedziałek – piątek 17:00-20:00<br />
-                  Sobota 15:00-17:00<br />
-                  <span class="text-gray-400"
-                    >(opłata zgodnie z cennikiem Twojego operatora
-                    telefonicznego)</span
-                  ></span
-                >
-              </p>
-            </div>
-          </div>
-        </div>
+        <p class="font-bold">
+          <span class="font-black"
+            >Telefoniczna Pierwsza Pomoc Psychologiczna</span
+          ><br />
+          <span class="text-gray-500"
+            ><a
+              class="text-primary hover:opacity-70 transition-opacity"
+              target="_blank"
+              href="https://www.qzmianom.org/index.php?page=Aktualnosci&id=3&SSID=2bf2cb03347e31cfe731a8eb8104c3a7&SSID=2bf2cb03347e31cfe731a8eb8104c3a7"
+              >tel. 22 425 98 48</a
+            ><br />
+            Poniedziałek – piątek 17:00-20:00<br />
+            Sobota 15:00-17:00<br />
+            <span class="text-gray-400"
+              >(opłata zgodnie z cennikiem Twojego operatora
+              telefonicznego)</span
+            ></span
+          >
+        </p>
         <div
           data-aos-delay="200"
           data-aos="fade-center"
-          class="flex-1 justify-end hidden md:block mt-0"
+          class="justify-center hidden md:flex mt-0"
         >
-          <img src="~/assets/img/telefon.svg" alt="" />
+          <img src="~/assets/img/sections/phones.svg" alt="" />
         </div>
-      </div>
-    </section>
+      </template>
+    </SectionWrapper>
   </main>
 </template>
