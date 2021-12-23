6860e5690e079bf1216fd9531bd180ceec7ebf79
bartoginski <bartoginski@outlook.com>
2021-12-23 14:15:26 +0100

    Update Symptoms component

--- components/sections/Symptoms.vue
+++ components/sections/Symptoms.vue
@@ -1,29 +1,44 @@
 <template>
-    <div class="bg-primary">
-        <div class="pb-2">
-            <RectangleHeader>Objawy depresji</RectangleHeader>
+  <SectionWrapper>
+    <template #title>
+      <RectangleHeader>Objawy depresji</RectangleHeader>
+    </template>
+    <template #content>
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-24">
+        <div>
+          <h3 class="md:text-6xl text-4xl pb-6">Biologiczne</h3>
+          <ul>
+            <li>Uczucie smutku, żal</li>
+            <li>Poczucie braku własnej wartości, bezsilność</li>
+            <li>Poczucie lęku i/lub winy</li>
+            <li>Płaczliwość, frustracja, drażliwość, wpadanie w gniew</li>
+            <li>Brak nadzei</li>
+            <li>Utrata chęci do życia</li>
+          </ul>
         </div>
-
-        <div class="flex justify-between text-white list-disc px-80">
-            <div class="text-3xl">
-                <ul class="list-disc list-inside"><h1 class="text-6xl">Biologiczne</h1>
-                    <li class="pt-4">Uczucie smutku, żal</li>
-                    <li class="pt-4">Poczucie braku własnej wartości, bezsilność</li>
-                    <li class="pt-4">Poczucie lęku i/lub winy</li>
-                    <li class="pt-4">Płaczliwość, frustracja, drażliwość, wpadanie w gniew</li>
-                    <li class="pt-4">Brak nadziei</li>
-                    <li class="pt-4">Utrata chęci do życia</li>
-                </ul>
-            </div>
-
-            <div class="text-3xl">
-                <ul class="list-disc list-inside"><h1 class="text-6xl">Psychologiczne</h1>
-                    <li class="pt-4">Wyraźny spadek lub wzrost wagi</li>
-                    <li class="pt-4">Bezsenność lub nadmierna senność</li>
-                    <li class="pt-4">Problemy z koncetracją</li>
-                    <li class="pt-4">Spadek sprwaności myślenia</li>
-                </ul>
-            </div>
+        <div>
+          <h3 class="md:text-6xl text-4xl pb-6">Psychologiczne</h3>
+          <ul>
+            <li>Wyraźny spadek lub wzrost wagi</li>
+            <li>Bezsenność lub nadmierna senność</li>
+            <li>Problemy z koncetracją</li>
+            <li>Spadek sprawności myślenia</li>
+          </ul>
         </div>
-    </div>
+      </div>
+    </template>
+  </SectionWrapper>
 </template>
+
+<style lang="scss" scoped>
+ul {
+    li {
+        @apply ml-8 relative pb-2 md:pb-4;
+        &:before {
+            content: '';
+            top: 5px;
+            @apply absolute h-4 w-4 md:h-5 md:w-5 bg-white rounded-full -left-8;
+        }
+    }
+}
+</style>
