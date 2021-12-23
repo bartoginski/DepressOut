ff77ca5f5573186c6cedc3c3fa8ab38baa7beb21
bartoginski <bartoginski@outlook.com>
2021-12-23 12:52:28 +0100

    Refactor /what page

--- components/sections/GetHelp.vue
+++ components/sections/GetHelp.vue
@@ -1,51 +1,26 @@
 <template>
-  <section>
-    <div
-      class="container flex-col flex md:items-center gap-0 md:gap-20 mt-20 mb-20"
-    >
-      <div
-        data-aos-delay="300"
-        data-aos="fade-down"
-        class="text-primary text-center md:text-left container"
-      >
-        <h2 class="text-4xl md:text-6xl text-center">Daj sobie pomóc</h2>
-      </div>
-      <div
-        class="gap-0 md:gap-40 text-primary text-center md:text-left justify-center md:justify-end flex-row flex md:items-left align-middle"
-      >
-        <div
-          data-aos-delay="300"
-          data-aos="fade-right"
-          class="text-primary text-center md:text-left flex-1 justify-center md:justify-end flex-row flex md:items-left gap-4 md:gap-0"
-        >
-          <div class="text-xl flex flex-col gap-10">
-            <p>
-              Depresja nie jest powodem do wstydu. Według badań WHO (Światowej
-              Organizacji Zdrowia) do 2030 roku depresja będzie najczęstszą
-              chorobą na świecie. Ponad to, depresja jest na czwartym miejscu
-              listy najpoważniejszych chorób i jedną z głównych przyczyn
-              samobójstw.
-            </p>
-
-            <p>
-              Wraz z całym zespołem działamy w pocie czoła, by stworzyć dla
-              użytkowników przyjemną przestrzeń, gdzie każdy będzie mógł znaleźć
-              pomoc. Czy to będzie informacyjny post na blogu, adres
-              specjalisty, czy nawet telefon pomocy w krytycznych sytuacjach.
-            </p>
-
-            <p>
-              *nazwa projektu* oferuje założenie prywatnego profilu, w którym
-              użytkownicy mogą śledzić swój nastrój w ciągu dnia, tygodnia a
-              nawet miesiąca, dzięki czemu łatwiej będzie rozpoznać stopień
-              zaawansowania depresji i podpowiedzieć najlepsze sposoby radzenia
-              sobie z problemem.
-            </p>
-          </div>
-        </div>
-          <img src="~/assets/img/what_gethelp.svg" alt="" data-aos-delay="300" data-aos="fade-left"
-          class="flex-1 justify-end hidden md:block align-middle" />
-      </div>
-    </div>
-  </section>
+  <SectionWrapperColumns class="text-primary">
+    <template #animated-title>Daj sobie pomóc</template>
+    <template #col-left>
+      <p>
+        Wraz z całym zespołem działamy w pocie czoła, by stworzyć dla
+        użytkowników przyjemną przestrzeń, gdzie każdy będzie mógł znaleźć
+        pomoc. Czy to będzie informacyjny post na blogu, adres specjalisty, czy
+        nawet telefon pomocy w krytycznych sytuacjach.
+      </p>
+      <p>
+        *nazwa projektu* oferuje założenie prywatnego profilu, w którym
+        użytkownicy mogą śledzić swój nastrój w ciągu dnia, tygodnia a nawet
+        miesiąca, dzięki czemu łatwiej będzie rozpoznać stopień zaawansowania
+        depresji i podpowiedzieć najlepsze sposoby radzenia sobie z problemem.
+      </p>
+    </template>
+    <template #col-right>
+      <img
+        class="hidden md:block"
+        src="~/assets/img/sections/what_gethelp.svg"
+        alt=""
+      />
+    </template>
+  </SectionWrapperColumns>
 </template>
--- components/sections/Why.vue
+++ components/sections/Why.vue
@@ -1,64 +1,26 @@
 <template>
-  <section>
-    <div class="container flex-col flex md:items-center gap-0 md:gap-20">
-        <div 
-        data-aos-delay="300"
-        data-aos="fade-down"
-        class="
-          text-white text-center
-          md:text-left  
-          container        
-        ">
-            <h2 class="text-4xl md:text-6xl text-center">Dlaczego stworzyliśmy ten projekt?</h2>
-        </div>
-      <div class="
-      gap-0 md:gap-40
-      text-white text-center
-          md:text-left
-          flex-1
-          justify-center
-          md:justify-end
-          flex-row flex
-          md:items-left
-      ">
-          <div
-        data-aos-delay="300"
-        data-aos="fade-right"
-        class="
-          text-white text-center
-          md:text-left
-          flex-1
-          justify-center
-          md:justify-end
-          flex-row flex
-          md:items-left
-          gap-4
-          md:gap-0
-        "
-      >
-        <div class="text-xl  flex flex-col gap-10">
-        <p>
-        Według ankiety, którą przeprowadziliśmy, aż 75% ankietowanych zna osobę dotkniętą depresją, 
-        z czego aż 94% ankietowanych to osoby w wieku 15-20 lat. Co więcej, 66,1% osób biorących udział w 
-        ankiecie odczuwa depresję lub podejrzewa ją u siebie. Ankieta została przeprowadzona wśród ponad 100 osób.
-        </p>
-
-        
-        <p>
-        Te wyniki pokazały nam, jak ważnym i aktualnym problemem jest depresja i że należy podjąć odpowiednie kroki, 
-        by pomós ludziom dotkniętym tą chorobą.
-        </p>
-        </div>
-      </div>
-      <div
-        data-aos-delay="300"
-        data-aos="fade-left"
-        class="flex-1 justify-end hidden md:block"
-      >
-        <img src="~/assets/img/what_wave_top.svg" alt="" />
-      </div>
-      </div>
-    </div>
-  </section>
+  <SectionWrapperColumns>
+    <template #animated-title> Dlaczego stworzyliśmy ten projekt? </template>
+    <template #col-left>
+      <p>
+        Według ankiety, którą przeprowadziliśmy, aż 75% ankietowanych zna osobę
+        dotkniętą depresją, z czego aż 94% ankietowanych to osoby w wieku 15-20
+        lat. Co więcej, 66,1% osób biorących udział w ankiecie odczuwa depresję
+        lub podejrzewa ją u siebie. Ankieta została przeprowadzona wśród ponad
+        100 osób.
+      </p>
+      <p>
+        Te wyniki pokazały nam, jak ważnym i aktualnym problemem jest depresja i
+        że należy podjąć odpowiednie kroki, by pomós ludziom dotkniętym tą
+        chorobą.
+      </p>
+    </template>
+    <template #col-right>
+      <img
+        class="hidden md:block"
+        src="~/assets/img/sections/why_created.svg"
+        alt=""
+      />
+    </template>
+  </SectionWrapperColumns>
 </template>
-
