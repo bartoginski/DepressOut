e4730f18bc7a8bd3671704593b4f07f8d1e55381
bartoginski <bartoginski@outlook.com>
2021-12-27 00:55:56 +0100

    Delete repeated text

--- components/sections/AboutDepression.vue
+++ components/sections/AboutDepression.vue
@@ -18,20 +18,6 @@
         (nie tylko dla lekarza) kwestią jest znajomość natury i objawów depresji
         oraz podstaw jej diagnostyki i leczenia.
       </p>
-      <p>
-        Według Światowej Organizacji Zdrowia (WHO) depresja jest wiodącą
-        przyczyną niesprawności i niezdolności do pracy na świecie oraz
-        najczęściej spotykanym zaburzeniem psychicznym. Choruje na nią w ciągu
-        całego życia kilkanaście procent populacji osób dorosłych. Choroba ta
-        dotyka dwa razy częściej kobiet. Co dziesiąty pacjent zgłaszający się do
-        lekarza pierwszego kontaktu z powodu innych dolegliwości ma
-        pełnoobjawową depresję, a drugie tyle osób cierpi z powodu pojedynczych
-        objawów depresyjnych. Niestety ponad połowa tych przypadków pozostaje
-        nierozpoznana, a spośród osób z rozpoznaną depresją jedynie połowa
-        otrzymuje adekwatne leczenie. Z tego względu niezwykle ważną dla każdego
-        (nie tylko dla lekarza) kwestią jest znajomość natury i objawów depresji
-        oraz podstaw jej diagnostyki i leczenia.
-      </p>
     </template>
   </SectionWrapper>
 </template>
