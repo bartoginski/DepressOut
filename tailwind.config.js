7ded486372f1aa573bc2dd3b7e4b9b9fec568554
bartoginski <bartoginski@outlook.com>
2021-12-30 17:24:54 +0100

    Fix MorePost slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,26 +4,27 @@ module.exports = {
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}'
+    './nuxt.config.{js,ts}',
   ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Nunito', 'sans-serif']
+        sans: ['Nunito', 'sans-serif'],
       },
       colors: {
-        'primary': '#28DCC3',
+        primary: '#28DCC3',
         'primary-light': '#5DF3DE',
-        'secondary': '#595dd4',
+        secondary: '#595dd4',
         'dark-gray': '#2F2E41',
         'light-gray': '#727272',
-        'dark-navy': '#302E45'
-      }
-    }
+        'dark-navy': '#302E45',
+      },
+      height: { 112: '28rem' },
+    },
   },
   variants: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 }
