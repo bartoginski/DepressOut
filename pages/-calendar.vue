8cb3036f55184e16ccabaee5087de586b630ed93
Bartosz Ogiński <bartoginski@outlook.com>
2022-02-01 17:45:43 +0100

    fix: disable calendar page

---
+++ pages/-calendar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <PageHeader>Kalendarz</PageHeader>
+    <CalendarGrid />
+    <HowFeeling />
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
