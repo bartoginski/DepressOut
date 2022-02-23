24a6110a444e3af7773b1d2cf87d189a6013fd20
Bartosz Ogiński <bartoginski@outlook.com>
2022-02-23 12:11:00 +0100

    add: quizz cta

---
+++ components/QuizCTA.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container" data-aos="fade-up">
+    <div
+      class="flex flex-col sm:flex-row sm:justify-between sm:items-center sm:gap-2 rounded-lg border-2 border-primary py-10 px-5"
+    >
+      <div class="">
+        <h2 class="text-2xl">Podoba ci się quiz?</h2>
+        <p class="text-lg">
+          Dowiedz się więcej o Międzynarodowym Dniu Walki z Depresją
+        </p>
+      </div>
+      <div class="flex justify-end mt-6 sm:mt-0">
+        <NuxtLink to="/blog/dzien-walki-z-depresja">
+          <ButtonPrimary>Zobacz więcej</ButtonPrimary></NuxtLink
+        >
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
--- components/quiz/QuizWrapper.vue
+++ components/quiz/QuizWrapper.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="flex w-full justify-center items-center antialiased text-gray-700 ">
-    <div class="flex  w-11/12 md:w-7/12 justify-center items-center">
+  <div
+    class="flex w-full justify-center items-center antialiased text-gray-700"
+  >
+    <div class="flex w-11/12 md:w-7/12 justify-center items-center">
       <div class="w-full max-x-xl">
         <div class="bg-white p-12 rounded-lg shadow-lg w-full">
           <div v-if="index < count">
@@ -8,46 +10,48 @@
               {{ questions[index]['question'] }}
             </p>
             <label
-              v-for="answer, key in questions[index]['answers']"
+              v-for="(answer, key) in questions[index]['answers']"
               :key="answer.id"
               :for="key"
               class="block mt-4 border border-grey-300 rounded-lg py-2 px-6 text-lg"
-              :class="{'hover:bg-gray-100 cursor-pointer' : selectedAnswer == '',
-              'bg-red-200' : selectedAnswer == key,
-              'bg-green-200' : key == questions[index]['correctAnswer'] && selectedAnswer != ''}"
-              >
-                <input
-                  :id="key"
-                  v-model="selectedAnswer"
-                  type="radio"
-                  class="hidden"
-                  :value="key"
-                  :disabled="selectedAnswer != ''"
-                  @change="answered($event)"
-                />
-                {{ answer }}
+              :class="{
+                'hover:bg-gray-100 cursor-pointer': selectedAnswer == '',
+                'bg-red-200': selectedAnswer == key,
+                'bg-green-200':
+                  key == questions[index]['correctAnswer'] &&
+                  selectedAnswer != '',
+              }"
+            >
+              <input
+                :id="key"
+                v-model="selectedAnswer"
+                type="radio"
+                class="hidden"
+                :value="key"
+                :disabled="selectedAnswer != ''"
+                @change="answered($event)"
+              />
+              {{ answer }}
             </label>
             <div class="mt-5 float-root">
               <button
-              v-show="selectedAnswer != '' && index < count - 1"
-              class="float-right px-5 py-2 bg-primary text-white text-sm font-bold tracking-wide rounded"
-              @click="nextQuestion"
+                v-show="selectedAnswer != '' && index < count - 1"
+                class="float-right px-5 py-2 bg-primary text-white text-sm font-bold tracking-wide rounded"
+                @click="nextQuestion"
               >
                 Następne &gt;
               </button>
               <button
-              v-show="selectedAnswer != '' && index == count -1"
-              class="float-right px-5 py-2 bg-primary text-white text-sm font-bold tracking-wide rounded"
-              @click="showResults"
+                v-show="selectedAnswer != '' && index == count - 1"
+                class="float-right px-5 py-2 bg-primary text-white text-sm font-bold tracking-wide rounded"
+                @click="showResults"
               >
                 Koniec
               </button>
             </div>
           </div>
           <div v-else>
-            <h2 class="text-bold 2xl text-3xl">
-              Wynik
-            </h2>
+            <h2 class="text-bold 2xl text-3xl">Wynik</h2>
             <div class="flex justify-start space-x-4 mt-6">
               <p>
                 Twoje poprawne odpowidzi to
@@ -60,9 +64,9 @@
             <button
               class="float-right px-5 py-2 bg-primary text-white text-sm font-bold tracking-wide rounded"
               @click="resetQuiz"
-              >
-                Jeszcze raz
-              </button>
+            >
+              Jeszcze raz
+            </button>
           </div>
         </div>
       </div>
@@ -72,39 +76,37 @@
 
 <script>
 import questions from '~/models/questions.js'
-  export default {
-    data() {
-      return {
-        index: 0,
-        selectedAnswer: '',
-        count: 10,
-        correctAnswers: 0,
-        questions
+export default {
+  data() {
+    return {
+      index: 0,
+      selectedAnswer: '',
+      count: 10,
+      correctAnswers: 0,
+      questions,
+    }
+  },
+  methods: {
+    answered(e) {
+      this.selectedAnswer = e.target.value
+      if (this.selectedAnswer === this.questions[this.index].correctAnswer) {
+        this.correctAnswers++
       }
     },
-    methods: {
-      answered(e) {
-        this.selectedAnswer = e.target.value;
-        if (this.selectedAnswer === this.questions[this.index].correctAnswer) {
-          this.correctAnswers++;
-        }
-      },
-      nextQuestion() {
-        this.index++;
-        this.selectedAnswer = '';
-      },
-      showResults() {
-        this.index++;
-      },
-      resetQuiz() {
-        this.index = 0;
-        this.selectedAnswer = '';
-        this.correctAnswers = 0;
-      }
-    }
-  }
+    nextQuestion() {
+      this.index++
+      this.selectedAnswer = ''
+    },
+    showResults() {
+      this.index++
+    },
+    resetQuiz() {
+      this.index = 0
+      this.selectedAnswer = ''
+      this.correctAnswers = 0
+    },
+  },
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
